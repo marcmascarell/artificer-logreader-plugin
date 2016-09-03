dcff18d7b157eb3b0c89214989679ea4704e9651
marcmascarell <marcmascarell@gmail.com>
2016-09-03 23:13:53 +0200

    = improved integration

--- src/LogReaderPlugin.php
+++ src/LogReaderPlugin.php
@@ -1,5 +1,6 @@
 <?php namespace Mascame\Artificer;
 
+use Mascame\Artificer\Extension\ResourceCollector;
 use Mascame\Artificer\Plugin\AbstractPlugin;
 
 
@@ -7,8 +8,9 @@ class LogReaderPlugin extends AbstractPlugin {
 
 	public $name = 'Log Reader';
 	public $description = 'The best (IMO) Log Viewer for Laravel 5 right in the admin interface';
-	public $author = 'Marc Mascarell';
+	public $author = 'Marc Mascarell (Credit: Rap2hpoutre)';
 	public $thumbnail = 'https://cloud.githubusercontent.com/assets/1575946/5243642/8a00b83a-7946-11e4-8bad-5c705f328bcc.png'; // url
+	public $slug = 'log-reader-plugin';
 
 	/**
 	 * Artificer does not know about your constructor so you
@@ -21,7 +23,7 @@ class LogReaderPlugin extends AbstractPlugin {
 	public function getRoutes() {
 		\Route::get('logs', [
 			'as' => 'admin.plugin.logreader',
-			'uses' => '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index'
+			'uses' => '\Mascame\Artificer\LogController@index'
 		]);
 	}
 
@@ -38,6 +40,18 @@ class LogReaderPlugin extends AbstractPlugin {
 			]
 		];
 	}
+
+    /**
+     * Extension config is not available until boot
+     *
+     * @param ResourceCollector $collector
+     * @return ResourceCollector
+     */
+    public function resources(ResourceCollector $collector) {
+        $collector->loadViewsFrom(__DIR__.'/../resources/views', $this->slug);
+
+        return $collector;
+    }
 
 	/**
 	 * This will be called if the plugin is installed
--- src/LogReaderPluginServiceProvider.php
+++ src/LogReaderPluginServiceProvider.php
@@ -1,30 +1,13 @@
 <?php namespace Mascame\Artificer;
 
-use Illuminate\Support\ServiceProvider;
 use Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider;
 
 
-class LogReaderPluginServiceProvider extends ServiceProvider {
+class LogReaderPluginServiceProvider extends ArtificerExtensionServiceProvider {
 
 	use ServiceProviderLoader;
 
 	public $package = 'mascame/artificer-logreader-plugin';
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-	}
 
 	/**
 	 * Register the service provider.
@@ -35,21 +18,11 @@ class LogReaderPluginServiceProvider extends ServiceProvider {
 	{
 		\App::bind(LogReaderPlugin::class);
 
-		\Mascame\Artificer\Artificer::pluginManager()->add($this->package, LogReaderPlugin::class);
+        $this->addPlugin(LogReaderPlugin::class);
 
 		$this->providers([
 			LaravelLogViewerServiceProvider::class
 		]);
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
 	}
 
 }
