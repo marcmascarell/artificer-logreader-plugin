ef03809d2f2ffb319781e732da745fd6cbf48253
marcmascarell <marcmascarell@gmail.com>
2016-09-03 23:15:27 +0200

    = improved integration

---
+++ src/LogController.php
@@ -0,0 +1,38 @@
+<?php namespace Mascame\Artificer;
+
+use Mascame\Artificer\Controllers\BaseController;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
+use Rap2hpoutre\LaravelLogViewer\LaravelLogViewer;
+
+
+class LogController extends BaseController
+{
+
+    public function index()
+    {
+
+        if (Request::input('l')) {
+            LaravelLogViewer::setFile(base64_decode(Request::input('l')));
+        }
+
+        if (Request::input('dl')) {
+            return Response::download(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('dl'))));
+        } elseif (Request::has('del')) {
+            File::delete(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('del'))));
+            return Redirect::to(Request::url());
+        }
+
+        $logs = LaravelLogViewer::all();
+
+        // The modified line
+        return View::make('log-reader-plugin::log', [
+            'logs' => $logs,
+            'files' => LaravelLogViewer::getFiles(true),
+            'current_file' => LaravelLogViewer::getFileName()
+        ]);
+    }
+}
