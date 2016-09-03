d51ec01005e8708e52b07a657f76db1ae3629253
marcmascarell <marcmascarell@gmail.com>
2016-09-03 23:20:30 +0200

    = improved integration

--- resources/views/log.blade.php
+++ resources/views/log.blade.php
@@ -16,18 +16,9 @@
 @overwrite
 
 @section('content')
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
 
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <style>
         .stack {
             font-size: 0.85em;
@@ -100,8 +91,7 @@
             </div>
         </div>
     </div>
-    <!-- We already got it from theme. jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>--}}
+
     <script src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
     <script>
