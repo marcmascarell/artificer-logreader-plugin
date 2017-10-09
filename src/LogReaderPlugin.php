23d38aa8d172dd06dfaac522d5e0fe6fc2da9b35
Marc Mascarell <marcmascarell@gmail.com>
2017-10-09 21:38:00 +0200

    Apply fixes from StyleCI (#7)

--- src/LogReaderPlugin.php
+++ src/LogReaderPlugin.php
@@ -2,9 +2,9 @@
 
 namespace Mascame\Artificer;
 
-use Mascame\Artificer\Assets\AssetsManagerInterface;
-use Mascame\Artificer\Extension\ResourceCollector;
 use Mascame\Artificer\Plugin\AbstractPlugin;
+use Mascame\Artificer\Extension\ResourceCollector;
+use Mascame\Artificer\Assets\AssetsManagerInterface;
 
 class LogReaderPlugin extends AbstractPlugin
 {
